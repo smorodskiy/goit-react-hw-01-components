4ff4097d8735163e06b550e0d4f9f5d8cc3c6c11
Igor Smorodsky <ingerard@gmail.com>
2022-09-23 22:56:44 +0300

    added desc and stats components

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,26 +1,85 @@
 // Check types of props
 import PropTypes from 'prop-types';
 
-// For stylish components
-// import styled from 'styled-components';
+import {
+  StatisticsList,
+  StatisticsItem,
+  DescriptionDiv,
+  Image,
+  Card,
+  Username,
+  Tag,
+  Location,
+} from './Profile.styled';
 
-import Description from './Description';
-import Statistics from './Statistics';
-
-// Profile
-export default function Profile(user) {
-  const { stats } = user;
+// Statistics ====================================
+function Statistics({ followers = 0, views = 0, likes = 0 }) {
   return (
-    <>
-      <Description {...user} />
-      <Statistics {...stats} />
-    </>
+    <StatisticsList>
+      <StatisticsItem>
+        <span>Followers</span>
+        <span>{followers}</span>
+      </StatisticsItem>
+      <StatisticsItem>
+        <span>Views</span>
+        <span>{views}</span>
+      </StatisticsItem>
+      <StatisticsItem>
+        <span>Likes</span>
+        <span>{likes}</span>
+      </StatisticsItem>
+    </StatisticsList>
   );
 }
 
-Profile.propTypes = {
-  user: PropTypes.shape({}),
-  stats: PropTypes.shape({}),
-};
+// Description ====================================
+function Description({
+  username,
+  tag,
+  location,
+  avatar = 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',  
+}) {
+  const fallbackAvatar = 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png';
+  return (
+    <DescriptionDiv>
+      <Image
+        src={avatar}
+        alt={tag}
+        onError={(event) =>
+          event.currentTarget.src = fallbackAvatar
+        }
+      />
+      <Username>{username}</Username>
+      <Tag>@{tag}</Tag>
+      <Location>{location}</Location>
+    </DescriptionDiv>
+  );
+}
 
-//   import user from 'path/to/user.json;
+// Profile ====================================
+export default function Profile({ username, tag, location, avatar, stats }) {
+  return (
+    <Card>
+      <Description
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+      />
+      <Statistics {...stats} />
+    </Card>
+  );
+}
+
+// Types
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
+};
