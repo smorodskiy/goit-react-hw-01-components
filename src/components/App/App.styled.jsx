ba813aba6df4ce39b8327e2da35e289188f9aa4f
Igor Smorodsky <ingerard@gmail.com>
2022-09-23 22:55:48 +0300

    Create App.styled.jsx

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,10 @@
+// For stylish components
+import styled from 'styled-components';
+
+// CSS in JS
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px;
+`;
