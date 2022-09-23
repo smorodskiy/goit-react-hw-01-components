c9cbda0544949d06e0349316e39e85abf180eb18
Igor Smorodsky <ingerard@gmail.com>
2022-09-23 22:58:19 +0300

    added ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { App } from 'components/App/App';
+
+import { ThemeProvider } from 'styled-components';
+import { theme } from './globalStyle';
+
+// Normalize style
+import GlobalStyle from './globalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
