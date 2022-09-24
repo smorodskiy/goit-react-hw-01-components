24c23431fd33c8c200bb07792e23d4cb30b5fc24
Igor Smorodsky <ingerard@gmail.com>
2022-09-24 16:17:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
-
 import { ThemeProvider } from 'styled-components';
-import { theme } from './globalStyle';
+import { theme } from 'constants';
 
 // Normalize style
 import GlobalStyle from './globalStyle';
