c13bbc561f2d997b25979b9da265804e289b26a8
Igor Smorodsky <ingerard@gmail.com>
2022-09-26 17:19:41 +0300

    fixed transaction component

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,42 +1,73 @@
 // Check types of props
-// import PropTypes from 'prop-types';
-// import { List, Image, Item, Status, Name } from './FriendList.styled';
+import PropTypes from 'prop-types';
+import {
+  TransactionSection,
+  Table,
+  ColumnData,
+  TableHead,
+  BodyRow,
+  HeadType,
+  BodyType,
+} from './TransactionHistory.styled';
 
-<table class="transaction-history">
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
+import { cellIn, cellOut } from './HoverEffect';
 
-  <tbody>
-    <tr>
-      <td>Invoice</td>
-      <td>125</td>
-      <td>USD</td>
-    </tr>
-    <tr>
-      <td>Withdrawal</td>
-      <td>85</td>
-      <td>USD</td>
-    </tr>
-  </tbody>
-</table>;
+function Transaction(data) {
+  const { type, amount, currency } = data;
+  return (
+    <BodyRow>
+      <ColumnData as={BodyType} className="column1" data-column="1">
+        {type}
+      </ColumnData>
+      <ColumnData className="column2" data-column="2">
+        {amount}
+      </ColumnData>
+      <ColumnData className="column3" data-column="3">
+        {currency}
+      </ColumnData>
+    </BodyRow>
+  );
+}
 
-export default function TransactionHistory({ transactions }) {
-  return <></>;
+export default function TransactionHistory({ items: history }) {
+  return (
+    <TransactionSection>
+      <Table onMouseOver={cellIn} onMouseOut={cellOut}>
+        <thead>
+          <tr>
+            <TableHead as={HeadType} className="column1">Type</TableHead>
+            <TableHead className="column2">Amount</TableHead>
+            <TableHead className="column3">Currency</TableHead>
+          </tr>
+        </thead>
+
+        <tbody>
+          {history.map(transaction => {
+            const { id, type, amount, currency } = transaction;
+
+            return (
+              <Transaction
+                key={id}
+                type={type}
+                amount={amount}
+                currency={currency}
+              />
+            );
+          })}
+        </tbody>
+      </Table>
+    </TransactionSection>
+  );
 }
 
 // Types
-// TransactionHistory.propTypes = {
-//   friends: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       avatar: PropTypes.string.isRequired,
-//       name: PropTypes.string.isRequired,
-//       isOnline: PropTypes.bool,
-//       id: PropTypes.number.isRequired,
-//     })
-//   ),
-// };
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
